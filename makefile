e1b8dd74e899f68f83942b48de01b86c2e4dadfb
Usuario user <nicholasseideman45@gmail.com>
2019-10-29 12:26:20 +0100

    no he hecho nada

---
+++ makefile
@@ -0,0 +1,16 @@
+jar:compile
+	jar cvfm catalogo.jar Manifest.txt -C bin .
+
+compile:clean
+	find src -name "*.java" | xargs javac -cp bin -d bin
+
+javadoc:
+	rm -rf html
+	mkdir html
+	find src -name "*.java" | xargs javadoc -d html -encoding utf-8 -docencoding utf-8 -charset utf-8
+
+clean:
+	rm -rf bin
+	mkdir bin
+
+
--- src/dominio/Ropa.java
+++ src/dominio/Ropa.java
@@ -4,29 +4,30 @@ public class Ropa{
 	private String marca;
 	private String tipo;
 
-public void setMarca(String Marca){
-	this.marca = marca;
-}
+        public void setMarca(String Marca){
+	    this.marca = marca;
+	}
 
-public String getMarca(){
-	return marca;
-}
+        public String getMarca(){
+	    return marca;
+        }  
 
-public void setTipo(String Tipo){
-	this.tipo = tipo;
-}
+        public void setTipo(String Tipo){
+	    this.tipo = tipo;
+        }
 
-public String getTipo(){
-	return tipo;
-}
+        public String getTipo(){
+	    return tipo;
+        }
 
-public Ropa(String marca, String tipo){
-	this.marca = marca;
-	this.tipo = tipo;
+        public Ropa(String marca, String tipo){
+	    this.marca = marca;
+	    this.tipo = tipo;
+	}
 
-@Override
-public String toString(){
-	return getTipo() + " " + getMarca();
-}
+        @Override
+        public String toString(){
+	    return getTipo() + " " + getMarca();
+        }
 
-        
+}  
